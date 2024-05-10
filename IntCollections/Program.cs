7183579c3e7ceb9b715bc5f67fc5288948cce206
Catarina Fuscaliuc <catarinafuscaliuc.dev@gmail.com>
2024-05-10 20:49:28 +0100

    IntCollections added

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IntCollections
 {
@@ -6,7 +7,21 @@ namespace IntCollections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> listTest = new List<int>();
+            Stack<int> stackTest = new Stack<int>();
+            Queue<int> queueTest = new Queue<int>();
+            HashSet<int> setTest = new HashSet<int>();
+
+            int[] numberArray = new int[] {1, 10, -30, 10, -5};
+
+            foreach (int number in numberArray)
+            {
+                listTest.Add(number);
+                stackTest.Push(number);
+                queueTest.Enqueue(number);
+                setTest.Add(number);
+
+            }
         }
     }
 }
