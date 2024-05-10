ed41da1d1ccebd369dc443bbf38dea169c7ccc7e
Catarina Fuscaliuc <catarinafuscaliuc.dev@gmail.com>
2024-05-10 20:56:23 +0100

    FilePower1 made

--- FilePower1/Program.cs
+++ FilePower1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace FilePower1
 {
@@ -6,7 +8,42 @@ namespace FilePower1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Por favor, forneça o nome do arquivo como argumento.");
+                return;
+            }
+
+            string fileName = args[0];
+            Queue<string> inputQueue = new Queue<string>();
+
+            Console.WriteLine("Digite as strings (pressione Enter para cada uma):");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                    break;
+
+                inputQueue.Enqueue(input);
+            }
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    foreach (string str in inputQueue)
+                    {
+                        writer.WriteLine(str);
+                    }
+                }
+
+                Console.WriteLine($"As strings foram salvas com sucesso no arquivo '{fileName}'.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro ao escrever no arquivo: {ex.Message}");
+            }
         }
     }
 }
